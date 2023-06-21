9de347fc247f16aefae9801b33795511fcff66b3
hawtim <vastfield@163.com>
2023-06-21 23:25:58 +0800

    add definePlugin

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import isSea from './SEA';
 import isBR from './BR';
 
 let a = null;
-if ('BR' === 'BR') {
+if (__REGION__ === 'BR') {
   a = isBR;
 } else {
   a = isSea;
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test:/\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         // 替换 webpack4 file-loader
-        type:'asset/resource'
-      }
-    ]
-  }
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __REGION__: JSON.stringify('BR'),
+    }),
+  ],
 };
